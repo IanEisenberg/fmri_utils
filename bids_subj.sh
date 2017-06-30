8d220ec2e2bf0983786eb62fc5aff962e28a9bde
Ian Eisenberg <ieisenbe@cnic13.stanford.edu>
2017-06-29 19:24:35 -0700

    changed organizer to transfer data as it goes, rather than subject by subject. Changed input arguments and record handling

--- bids_subj.sh
+++ bids_subj.sh
@@ -6,20 +6,17 @@ destination=sherlock:/oak/stanford/groups/russpold/data/uh2
 record=/data/uh2/sherlock_completed_files.txt
 
 # loop through all uh2 files
-for f in /nimsfs/russpold/uh2/*
-do
-	if grep -Fxq "$f" $record; then
-		echo $f already transferred
-	else		
-		# run bids_organizer, which moves files to /data/uh2
-		python bids_organizer.py  $f --rsync_output test --id_correction uh2_id_correction.json --record $record
+f=/nimsfs/russpold/uh2/*$1*
+if grep -Fxq "$f" $record; then
+	echo $f already transferred
+else		
+	# run bids_organizer, which moves files to /data/uh2
+	python bids_organizer.py  $f --rsync_output test --id_correction uh2_id_correction.json --record $record
 
-		rm -r /data/uh2/sub*
-	fi
-done
+	rm -r /data/uh2/sub*
+fi
 
 sort $record -o $record
-
 
 # move non subject specific folders (e.g.task meta data)
 # rsync -avz --progress /data/uh2/task* $destination
